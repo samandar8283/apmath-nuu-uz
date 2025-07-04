857a716979bd56cff48a1d98d507d811024490cc
samandar8283 <samandar8283@gmail.com>
2025-07-04 19:36:02 +0500

    first commit

---
+++ public/assets/js/en-ru-scripts.js
@@ -0,0 +1,15 @@
+// === Languages js start ===
+document.querySelectorAll(".language[id]").forEach(function(langBtn) {
+    langBtn.addEventListener("click", function () {
+        const selectedLang = this.id;
+        const pageClass = document.querySelector("aside")?.className.split(" ")[0];
+
+        // if (!pageClass) return;
+
+        let langPath = selectedLang === "uz" ? "./../" : "./../" + selectedLang + "/";
+        let targetUrl = langPath + pageClass + ".html";
+
+        window.location.href = targetUrl;
+    });
+});
+// === /Languages js end ===
